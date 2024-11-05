1484fac85f14d337734d2a6b6c8910d523357870
Umang Raval <umangraval0365@gmail.com>
2024-11-05 14:00:51 +0530

    major update

--- server/app.js
+++ server/app.js
@@ -7,13 +7,17 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//resolving  dirname in ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+
 const port = process.env.PORT || 8800;
 const app = express();
 
-// app.use({
-//   origin: "url of front-end that you hosted",
-//   credentials: true
-//   })
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -31,35 +35,14 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
+//use client app
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+//render client for any path
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
+
 app.listen(port, () => {
   console.log("Server is running!");
 });
-
-// import express from "express";
-// import cors from "cors";
-// import postRoute from "./routes/post.route.js";
-// import authRoute from "./routes/auth.route.js";
-// import testRoute from "./routes/test.route.js";
-// import userRoute from "./routes/user.route.js";
-// import chatRoute from "./routes/chat.route.js";
-// import messageRoute from "./routes/message.route.js";
-// import cookieParser from "cookie-parser";
-
-// const app = express();
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/posts", postRoute);
-// app.use("/api/test", testRoute);
-// app.use("/api/chats", chatRoute);
-// app.use("/api/messages", messageRoute);
-
-// app.listen(8800, () => {
-//   console.log("Server is running ");
-// });
